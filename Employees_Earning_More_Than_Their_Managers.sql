a03cad0f8ba1b0965fcd248b7b91b83098c71e8e
Naveen Karanth <naveen.karantha@bt.com>
2022-09-14 10:04:35 +0530

    Commit on 14-Sept-2022

---
+++ Employees_Earning_More_Than_Their_Managers.sql
@@ -0,0 +1,24 @@
+/* Question */
+/* 
+Write an SQL query to find the employees who earn more than their managers.
+Return the result table in any order.
+
+Table: Employee
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| salary      | int     |
+| managerId   | int     |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table indicates the ID of an employee, their name, salary, and the ID of their manager.
+*/
+
+-- Solution
+SELECT e1.name as Employee
+FROM Employee e1 LEFT JOIN Employee e2
+ON e1.managerId = e2.id
+WHERE e1.salary>e2.salary
