90615befa145ea4e40764394738fa3cb1b3e67e5
Naveen Karanth <naveenfaster@gmail.com>
2022-09-19 11:24:47 +0530

    Commit on 19-Sept-2022

---
+++ Classes_More_Than_5_Students.sql
@@ -0,0 +1,24 @@
+/* Question */
+/* 
+
+Write an SQL query to report all the classes that have at least five students.
+Return the result table in any order.
+
+Table: Courses
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| student     | varchar |
+| class       | varchar |
++-------------+---------+
+(student, class) is the primary key column for this table.
+Each row of this table indicates the name of a student and the class in which they are enrolled.
+*/
+
+-- Solution
+
+SELECT class
+FROM Courses
+GROUP BY class
+HAVING COUNT(student) >= 5
