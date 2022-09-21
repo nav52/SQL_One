38d2667930aa4c6224b9e4a5734a70e2fa06d44b
Naveen Karanth <naveenfaster@gmail.com>
2022-09-21 09:55:12 +0530

    Commit on 21-Sept-2022

---
+++ Actors_and_Directors_Who_Cooperated_At_Least_Three_Times.sql
@@ -0,0 +1,23 @@
+/* Question */
+/* 
+Write a SQL query for a report that provides the pairs (actor_id, director_id) where the actor has cooperated with the director at least three times.
+Return the result table in any order.
+
+Table: ActorDirector
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| actor_id    | int     |
+| director_id | int     |
+| timestamp   | int     |
++-------------+---------+
+timestamp is the primary key column for this table.
+*/
+
+-- Solution
+
+SELECT actor_id, director_id
+FROM ActorDirector
+GROUP BY actor_id, director_id
+HAVING COUNT(timestamp) >= 3
