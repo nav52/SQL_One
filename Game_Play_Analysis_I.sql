15343cc9014142b100d3e3ee58739e42963bfdc8
Naveen Karanth <naveenfaster@gmail.com>
2022-09-22 09:09:11 +0530

    Commit on 22-Sept-2022

---
+++ Game_Play_Analysis_I.sql
@@ -0,0 +1,25 @@
+/* Question */
+/* 
+Write an SQL query to report the first login date for each player.
+Return the result table in any order.
+
+Table: Activity
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| player_id    | int     |
+| device_id    | int     |
+| event_date   | date    |
+| games_played | int     |
++--------------+---------+
+(player_id, event_date) is the primary key of this table.
+This table shows the activity of players of some games.
+Each row is a record of a player who logged in and played a number of games (possibly 0) before logging out on someday using some device.
+*/
+
+-- Solution
+
+SELECT player_id, MIN(event_date) AS 'first_login'
+FROM Activity
+GROUP BY player_id
