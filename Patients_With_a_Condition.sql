ae442f7d48336c73739af1f2aaf3d1e696a36c2d
Naveen Karanth <naveenfaster@gmail.com>
2022-09-27 11:14:49 +0530

    Commit on 27-Sept-2022

---
+++ Patients_With_a_Condition.sql
@@ -0,0 +1,25 @@
+/* Question */
+/* 
+Write an SQL query to report the patient_id, patient_name all conditions of patients who have Type I Diabetes. 
+Type I Diabetes always starts with DIAB1 prefix
+Return the result table in any order.
+
+Table: Patients
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| patient_id   | int     |
+| patient_name | varchar |
+| conditions   | varchar |
++--------------+---------+
+patient_id is the primary key for this table.
+'conditions' contains 0 or more code separated by spaces. 
+This table contains information of the patients in the hospital.
+*/
+
+-- Solution
+
+SELECT *
+FROM Patients
+WHERE conditions LIKE "DIAB1%" OR conditions LIKE "% DIAB1%"
