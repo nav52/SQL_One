a91841a2969a927eb3f4427e6eab63934e9d9c34
Naveen Karanth <naveenfaster@gmail.com>
2022-09-30 09:48:45 +0530

    Commit on 30-Sept-2022

---
+++ Find_Followers_Count.sql
@@ -0,0 +1,23 @@
+/* Question */
+/* 
+Write an SQL query that will, for each user, return the number of followers.
+Return the result table ordered by user_id.
+
+Table: Followers
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| user_id     | int  |
+| follower_id | int  |
++-------------+------+
+(user_id, follower_id) is the primary key for this table.
+This table contains the IDs of a user and a follower in a social media app where the follower follows the user.
+*/
+
+-- Solution
+
+SELECT user_id, COUNT(DISTINCT follower_id) AS 'followers_count'
+FROM Followers
+GROUP BY 1
+ORDER BY 1
