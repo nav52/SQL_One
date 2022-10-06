a9cff0589967ad0e844abe0cd6e0451bef58da48
Naveen Karanth <naveenfaster@gmail.com>
2022-10-06 08:28:37 +0530

    Commit on 06-Oct-2022

---
+++ Calculate_Special_Bonus.sql
@@ -0,0 +1,30 @@
+/* Question */
+/* 
+Write an SQL query to calculate the bonus of each employee. 
+The bonus of an employee is 100% of their salary if the ID of the employee is an odd number 
+and the employee name does not start with the character 'M'. 
+The bonus of an employee is 0 otherwise.
+Return the result table ordered by employee_id.
+
+Table: Employees
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| name        | varchar |
+| salary      | int     |
++-------------+---------+
+employee_id is the primary key for this table.
+Each row of this table indicates the employee ID, employee name, and salary.
+*/
+
+-- Solution
+
+SELECT employee_id,
+    CASE 
+        WHEN employee_id%2<>0 AND NOT name LIKE 'M%' THEN salary
+        ELSE 0
+    END AS 'bonus'
+FROM Employees
+ORDER BY 1
