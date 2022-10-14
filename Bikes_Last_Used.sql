9f93be48fbb527e613cd88f2b98fbf1fbc9c315b
Naveen Karanth <naveenfaster@gmail.com>
2022-10-14 12:48:16 +0530

    Commit on 14-Oct-2022

---
+++ Bikes_Last_Used.sql
@@ -0,0 +1,28 @@
+/* Question */
+/* 
+Find the last time each bike was in use. 
+Output both the bike number and the date-timestamp of the bike's last use (i.e., the date-time the bike was returned). 
+Order the results by bikes that were most recently used.
+
+Table: dc_bikeshare_q1_2012
+
+duration:           varchar
+duration_seconds:   int
+start_time:         datetime
+start_station:      varchar
+start_terminal:     int
+end_time:           datetime
+end_station:        varchar
+end_terminal:       int
+bike_number:        varchar
+rider_type:         varchar
+id:                 int
+
+*/
+
+-- Solution
+
+SELECT bike_number, MAX(end_time) as 'last_use'
+FROM dc_bikeshare_q1_2012
+GROUP BY 1
+ORDER BY 2 DESC
