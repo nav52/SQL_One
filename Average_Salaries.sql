60f6c370fdd12d2df51f3cd7dc9923c1fa8415d8
Naveen Karanth <naveenfaster@gmail.com>
2022-10-20 11:01:59 +0530

    Commit on 20-Oct-2022

---
+++ Average_Salaries.sql
@@ -0,0 +1,31 @@
+/* Question */
+/* 
+Compare each employee's salary with the average salary of the corresponding department.
+Output the department, first name, and salary of employees along with the average salary of that department.
+
+Table: employee
+
+id:             int
+first_name:     varchar
+last_name:      varchar
+age:            int
+sex:            varchar
+employee_title: varchar
+department:     varchar
+salary:         int
+target:         int
+bonus:          int
+email:          varchar
+city:           varchar
+address:        varchar
+manager_id:     int
+
+*/
+
+-- Solution
+
+SELECT department, 
+    first_name, 
+    salary, 
+    AVG(salary) OVER (PARTITION BY Department) AS 'avg_salary'
+FROM employee;
